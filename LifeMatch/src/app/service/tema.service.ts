ae9b21ab892ff8e703bea98644d0f661463d81a3
Wellington Vieira <wellington.vieira@fatec.sp.gov.br>
2020-12-11 09:33:40 -0300

    inicio get postagem

--- LifeMatch/src/app/service/tema.service.ts
+++ LifeMatch/src/app/service/tema.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //atribuindo a variável token o objeto HttpHeaders para autorizar a consulta.
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token')!)
+  }
+
+  //Método responsável por retornar as temas.
+  getAllTemas():Observable<Tema[]>{
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number): Observable<Tema> {
+    return this.http.delete<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
 }
